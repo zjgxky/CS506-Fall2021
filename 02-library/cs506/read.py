dcc377e366cbf56e79a4032680267bc96affd0d4
Kaiyan Xu <xukaiyan@dhcp-wifi-8021x-155-41-76-72.bu.edu>
2021-10-21 21:22:00 -0400

    previous lab

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,10 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    with open(csv_file_path, 'r') as f:
+        csv = []
+        for line in f:
+            csv.append([x for x in line.split(",") if x != "\n"])
+
+    return csv
+
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -5,12 +5,43 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    res =  0
+    for i in range(len(x)):
+        v = (x[i] - y[i])
+        if v < 0:
+            res += v * (-1)
+        else:
+            res += v
+    return res 
+
+def intersect(ls1, ls2):
+    ls3 = [x for x in ls1 if x in ls2]
+    return ls3
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    intersection = list(set(x).intersection(y))
+    intersectionCardinality = len(intersection)
+    unionCardinality = (len(x) + len(y)) - intersectionCardinality
+    if unionCardinality == 0:
+        return 0
+    return float(intersectionCardinality) / unionCardinality
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    dot = 0
+    for i in x:
+        dot += x[i]*y[i]
+
+    xlen = 0
+    ylen = 0
+    for i in x:
+        xlen += x[i]**2
+        ylen += y[i]**2
+    xlen = xlen**(1/2)
+    ylen = ylen**(1/2)
+    cross = xlen * ylen
+    if cross == 0:
+        return 'undefined'
+    else:    
+        return dot / cross
 
 # Feel free to add more
